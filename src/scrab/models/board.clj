16fd2330f74ea950e2d5f9376dcd4bc23a81fc89
Rory McCann <tirard22@googlemail.com>
2012-03-17 14:37:27 +0000

    hmm

--- src/scrab/models/board.clj
+++ src/scrab/models/board.clj
@@ -1,42 +1,41 @@
-(def test-graph {1 {:tiles [] :start [1 1] :end [1 5] :neighbours [{:word 2 :intersection 2}]}
+(def test-graph {1 {:tiles [] :start [1 1] :end [5 1] :neighbours [{:word 2 :intersection 2}]}
                  2 {:tiles [] :start [3 1] :end [3 4] :neighbours [{:word 1 :intersection 0}]}})
 
-(def max-key (reduce #(max %1 %2) (keys test-graph)))
+(defn max-key [graph] (reduce #(max %1 %2) (keys graph)))
 
-; TODO intersects
-; returns true? or returns value?
-                                        ; use cond-let maybe, to bind to intersects
+(defn end-points-from-tiles [word-tiles]
+  (let [x1 (first ((first word-tiles) :position))
+        x2 (first ((first word-tiles)
+                    :position))
+        y1 (last ((first word-tiles)
+                   :position))
+        y2 (last ((last word-tiles)
+                  :position))]    
+    [x1 y1 x2 y2]))
 
-(defn coords [word other-word]
+(defn end-points-from-word [word]
   (let [x1 (first (word :start))
         x2 (first (word :end))
         y1 (last (word :start))
-        y2 (last (word :end))
-        x3 (first (other-word :start))
-        x4 (first (other-word :end))
-        y3 (last (other-word :start))
-        y4 (last (other-word :end))]
-    [x1 y1 x2 y2 x3 y3 x4 y4]))
+        y2 (last (word :end))]    
+     [x1 y1 x2 y2]))
 
-(defn intersects [x1 y1 x2 y2 x3 y3 x4 y4]
-  (let [denom (- (* (- y4 y3)
-                    (- x2 x1))
-                 (* (- x4 x3)
-                    (- y2 y1)))
-        ua-num (- (* (- x4 x3)
-                     (- y1 y3))
-                  (* (- y4 y3)
-                     ( - x1 x3)))
+(defn intersects [word-tiles word]
+  (let [[x1 y1 x2 y2] (end-points-from-tiles word-tiles)
+        [x3 y3 x4 y4] (end-points-from-word word)
+        denom (- (* (- y4 y3)
+                    (- x2 x1)))
+        ua-num (- (* (- x4 x3) (- y1 y3)) (* (- y4 y3) (- x1 x3)))
         ub-num (- (* (- x2 x1)
                      (- y1 y3))
                   (* (- y2 y1)
-                     (- x1 x3)))]
+                     (- x1 x3)))]        
     (cond
-      (and (= 0 denom)
-           (= 0 ua-num)
-           (= 0 ub-num))
+      (and (zero? denom)
+           (zero? ua-num)
+           (zero? ub-num))
       [x1 y1]      
-      (= 0 denom) nil
+      (zero? denom) nil
       :else
       (let [ua (/ ua-num denom)
             ub (/ ub-num denom)]
@@ -46,31 +45,61 @@
            (+ y1 (* ua (- y2 y1)))] 
           nil)))))
 
-                                        ; works, uses word structure similar to in graph
-
-(defn find-neighbours [graph word]
+;; works, uses word structure similar to in graph
+(defn find-neighbours [graph word-tiles]
   (loop [current-word (first graph)
          rest (next graph)
          neighbours []]
     (cond
-      (not current-word) neighbours 
-      (apply intersects (coords word (val current-word))) ; almost there -
-      ; need to cons right value
+      (not current-word) neighbours
+      (intersects word-tiles (val current-word))
       (recur (first rest)
              (next rest)
-             (cons
+             (conj
+              neighbours
               {:word (key current-word)
-               :intersection (apply intersects (coords word (val current-word)))}
-              neighbours))
+               :intersection (intersects word-tiles (val current-word))}))
       :else (recur (first rest) (next rest) neighbours))))
 
                                         ; Assumes word is in the right order
                                         ; what is the word structure?
 
-(defn add-word [graph word]
-  (let [i (+ 1 (max-key graph))
-        start ((first word) :position)
-        end ((last word) :position)
-        neighbours (find-neighbours graph word)]
-    (into graph {i {:tiles (word :tiles), :start start, :end end, :neighbours neighbours}})))
+(defn add-word [graph word-tiles]
+  (let [i (inc (max-key graph))
+        start ((first word-tiles) :position)
+        end ((last word-tiles) :position)
+        neighbours (find-neighbours graph word-tiles)
+        word {i {:tiles word-tiles, :start start, :end end, :neighbours neighbours}}]
+    (loop [neighbour (graph ((first neighbours) :word))
+           next-neighbours (rest neighbours)
+           neighbour-key ((first neighbours) :word)
+           new-graph {}]
+      (if neighbour
+        (recur (graph (first next-neighbours))
+               (next next-neighbours)
+               ((first next-neighbours) :word)
+               (conj new-graph
+                     {neighbour-key {:tiles (neighbour :tiles)
+                                     :start (neighbour :start)
+                                     :end (neighbour :end)
+                                     :neighbours (conj (neighbour :neighbours)
+                                                       {:word i
+                                                        :intersection (get-inter
+                                       }
+                     ))
+        new-graph
+        ))))
 
+
+
+;; Checks that no square is occupied more than once
+(defn check-valid-graph [graph]
+  (let [board (empty-board)]
+    (loop [;TODO
+           ])))
+
+
+;; Checks that all the words are real words
+(defn check-valid-word [graph]
+  ;TODO
+  True)
